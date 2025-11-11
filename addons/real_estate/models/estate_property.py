2d27d41d013f721469b3db13d04c017bc9197d1a
Lester Aplicano <aplicano71@gmail.com>
2025-11-11 08:33:29 -0600

    Agregando un SQL constraints de, valor negativo, no repetir nombres y que no puede quedar vacio un campo

--- addons/real_estate/models/estate_property.py
+++ addons/real_estate/models/estate_property.py
@@ -7,3 +7,9 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Nombre", required=True)
     price = fields.Float(string="Precio")
     active = fields.Boolean(string="Activo", default=True)
+
+    _sql_constraints = [
+        ('no_number_negative', 'CHECK(price >= 0)', "El numero esperado debe ser positivo"),
+        ('name_not_null', 'CHECK(name IS NOT NULL)', "El nombre no puede estar vacio"),
+        ('name_uniq', 'UNIQUE(name)',"El nombre de la clasificacion ya existe"),
+    ] 
--- addons/real_estate/models/ventas_totales.py
+++ addons/real_estate/models/ventas_totales.py
@@ -7,4 +7,8 @@ class VentasTotales(models.Model):
 
     valor = fields.Float(string="Valor de las ventas totales")
 
-    
+    _sql_constraints = [
+        ('name_not_null', 'CHECK(valor IS NOT NULL)', "El nombre no puede estar vacio"),
+        ('no_number_negative', 'CHECK(valor >= 0)', "El numero esperado debe ser positivo"),
+        ('name_uniq', 'UNIQUE(valor)',"El nombre de la clasificacion ya existe"),
+    ] 
